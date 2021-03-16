b0b0e638dd1bf26969485b5054d95c27352cab00
roman <klilukqq@gmail.com>
2021-03-16 15:32:07 +0300

    Lab2

--- Lab2/TestsLab2.cpp
+++ Lab2/TestsLab2.cpp
@@ -156,6 +156,42 @@ namespace TestsLab2
 			Assert::IsTrue(Method(20, 20, 10, 9, 0, 0) == "Are you kidding me?");
 			Logger::WriteMessage(L"final passed!");
 		}
+		
+		//ложные тесты
 
+		TEST_METHOD(Additional_test_TEST1)
+		{
+			Logger::WriteMessage(L"A_TEST1 (20, 20, 10, 10, 0, 0)");
+			Assert::IsTrue(Method(0, 0, 0, 0, 0, 0) != "1 ", L"Assert");
+			Logger::WriteMessage(L"A_TEST1 passed!");
+		}
+
+		TEST_METHOD(Additional_test_TEST2)
+		{
+			Logger::WriteMessage(L"A_TEST2 (20, 20, 10, 10, 0, 0)");
+			Assert::IsTrue(Method(5, 6, 1, 3, 0, 1) != "2 " + to_string(-1.0) + " " + to_string(2.0), L"Assert");
+			Logger::WriteMessage(L"A_TEST2 passed!");
+		}
+
+		TEST_METHOD(Additional_test_TEST3)
+		{
+			Logger::WriteMessage(L"A_TEST3 (20, 20, 10, 10, 0, 0)");
+			Assert::IsTrue(Method(1, 2, 3, 4, 5, 6) != "0 " + to_string(0.0) + " " + to_string(0.35), L"Assert");
+			Logger::WriteMessage(L"A_TEST3 passed!");
+		}
+
+		TEST_METHOD(Additional_test_TEST4)
+		{
+			Logger::WriteMessage(L"A_TEST4 (20, 20, 10, 10, 0, 0)");
+			Assert::IsTrue(Method(1, 0, 1, 0, 3, 0) != "3 " + to_string(-5.0) + " " + to_string(0.0), L"Assert");
+			Logger::WriteMessage(L"A_TEST4 passed!");
+		}
+
+		TEST_METHOD(Additional_test_TEST5)
+		{
+			Logger::WriteMessage(L"A_TEST5 (20, 20, 10, 10, 0, 0)");
+			Assert::IsTrue(Method(15, 15, 15, 15, 15, 15) != "2 " + to_string(-4.0) + " " + to_string(1.0), L"Assert");
+			Logger::WriteMessage(L"A_TEST5 passed!");
+		}
 	};
 }
